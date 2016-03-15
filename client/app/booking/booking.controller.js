e8d20ce3b23a467d87c8dcbb02b9956a3342a1d8
DJ Meyers <mrdjmeyers@ufl.edu>
2016-03-14 20:52:29 -0400

    Made a bunch of changes with Angular.  Code doesn't suck as much now.  Still needs to fix the layout of the form.

--- client/app/booking/booking.controller.js
+++ client/app/booking/booking.controller.js
@@ -1,6 +1,25 @@
 'use strict';
 
 angular.module('fmgApp')
-  .controller('BookingCtrl', function ($scope) {
-    $scope.message = 'Hello';
-  });
+  .controller('bookingForm', ['$scope', '$http', function($scope, $http) {
+     function resetForm() {
+        $scope.propName = '';
+        $scope.phoneNum = '';
+        $scope.email = '';
+        $scope.eventDate = '';
+        $scope.eventType = '';
+        $scope.descript = '';
+     }
+
+    $scope.submit = function() {
+      if ( $scope.bookingForm.propName && $scope.bookingForm.phoneNum && $scope.bookingForm.email && $scope.bookingForm.eventDate && $scope.bookingForm.eventType && $scope.bookingForm.descript ) {
+          // $http.post('/someUrl', data, config).then(successCallback, errorCallback);
+          $http.post('/api/bookingForm', $scope.bookingForm).then( function(data) {
+              console.log('SUCCESS');
+              resetForm();
+              // May want to redirect somewhere after success, or take the received data back, and display it somehow?
+            }, function(err) { console.log(err); }
+          );
+      }
+    };
+  }]);
