fc9b11c238786a021c4388f47f703c59758323dd
DJ Meyers <mrdjmeyers@ufl.edu>
2016-03-21 14:29:15 -0400

    Committing so Gavin can check stuff out.

--- server/api/bookingForm/bookingForm.controller.js
+++ server/api/bookingForm/bookingForm.controller.js
@@ -14,6 +14,19 @@ import BookingForm from './bookingForm.model';
 var nodemailer = require('nodemailer');
 var xoauth2 = require('xoauth2');
 
+var generator = xoauth2.createXOAuth2Generator({
+    user: 'forcemediagroupdb@gmail.com',
+    clientId: '1049619144734-uun0kb3kebr8coi3jrs3aqgp17gfcop8.apps.googleusercontent.com',
+    clientSecret: '-iNcYB0V2sStaADp3zOrQ3L3',
+    refreshToken: '1/HXOOpKwTS3-oztzSxIwcMTph5awFSpkp-UUtuZRAJM0MEudVrK5jSpoR30zcRFq6',
+    accessToken: 'ya29.pwLTEs8BGzTkM1MZE1S40NMp_732Y1aSyVlcqv1iPz0aIyhfT7MIYUzS2HHPio3m2Q'
+});
+
+generator.on('token', function(token){
+    console.log('New token for %s: %s', token.user, token.accessToken);
+});
+
+
 function respondWithResult(res, statusCode) {
   statusCode = statusCode || 200;
   return function(entity) {
@@ -103,21 +116,13 @@ export function destroy(req, res) {
     .catch(handleError(res));
 }
 
-
 // create reusable transporter object using the default SMTP transport
-
 //Need to refresh accessToken which changes every hour.
 // login
 var transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        xoauth2: xoauth2.createXOAuth2Generator({
-            user: 'forcemediagroupdb@gmail.com',
-            clientId: '1049619144734-uun0kb3kebr8coi3jrs3aqgp17gfcop8.apps.googleusercontent.com',
-            clientSecret: '-iNcYB0V2sStaADp3zOrQ3L3',
-            refreshToken: '1/HXOOpKwTS3-oztzSxIwcMTph5awFSpkp-UUtuZRAJM0MEudVrK5jSpoR30zcRFq6',
-            accessToken: 'ya29.pwLTEs8BGzTkM1MZE1S40NMp_732Y1aSyVlcqv1iPz0aIyhfT7MIYUzS2HHPio3m2Q'
-        })
+        xoauth2: generator
     }
 });
 // setup e-mail data with unicode symbols
